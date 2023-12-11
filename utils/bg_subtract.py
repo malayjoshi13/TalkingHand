505482e40d88777534d6b543696e7cc0da6ffb4e
Malay Joshi <71775151+malayjoshi13@users.noreply.github.com>
2023-12-12 03:06:11 +0530

    Delete utils directory

--- utils/bg_subtract.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import cv2
-
-def removeBG(frame, bgModel, learningRate):
-    fgmask = bgModel.apply(frame,learningRate=learningRate)
-    kernel = np.ones((3, 3), np.uint8)
-    fgmask = cv2.erode(fgmask, kernel, iterations=1)
-    res = cv2.bitwise_and(frame, frame, mask=fgmask)
-    return res
